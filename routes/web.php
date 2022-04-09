a4929882a0a2a4d459a0651595d6d9547ea31671
Amadeo Santos <92321680+amadeoSantos@users.noreply.github.com>
2022-04-08 22:29:37 -0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CasillaController;
+use App\Http\Controllers\CandidatoController;
+use App\Http\Controllers\EleccionController;
+use App\Http\Controllers\VotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('casilla', CasillaController::class);
+Route::resource('candidato',CandidatoController::class);
+Route::resource('eleccion',EleccionController::class);
+Route::resource('voto', VotoController::class);
