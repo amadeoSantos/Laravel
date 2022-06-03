89cd4c12998972e95c7fc9c091e0b3faf5ada526
Amadeo Santos <92321680+amadeoSantos@users.noreply.github.com>
2022-06-03 10:42:20 -0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PDFController;
 use App\Http\Controllers\CasillaController;
 use App\Http\Controllers\CandidatoController;
-use App\Http\Controllers\EleccionController;
 use App\Http\Controllers\VotoController;
+use App\Http\Controllers\EleccionController;
+use App\Http\Controllers\Auth\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +23,34 @@ use App\Http\Controllers\VotoController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('preview', 'App\Http\Controllers\PDFController@preview');
+Route::get('download', 'App\Http\Controllers\PDFController@download')->name('download');
+
+Route::get('casilla/pdf', [CasillaController::class,'generatepdf']);
+
+
 Route::resource('casilla', CasillaController::class);
-Route::resource('candidato',CandidatoController::class);
-Route::resource('eleccion',EleccionController::class);
+Route::resource('candidato', CandidatoController::class);
 Route::resource('voto', VotoController::class);
+Route::resource('eleccion', EleccionController::class);
+
+Route::get('login',[LoginController::class,'index'] ) -> name('login');
+
+
+Route::get('/login/facebook','App\Http\Controllers\Auth\LoginController@redirectToFacebookProvider');
+Route::get('/login/facebook/callback','App\Http\Controllers\Auth\LoginController@handleProviderFacebookCallback');
+Route::get('logout',[LoginController::class, 'logout']);
+Route::get('/menu',function(){
+     return view('menu');
+});
+
+/*Route::middleware(['auth'])->group(function(){
+    Route::resource('voto', VotoController::class);
+
+});*/
+
+Route::get('generate-pdf', [PDFController::class, 'generatePDF']);
+
+
+
+
